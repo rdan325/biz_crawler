b2b1f10ca329333f2787fe011c661136794ffb60
Ryan Anderson <randerson325@comcast.net>
2021-07-05 20:48:51 -0500

    Finish crawl work

--- crawler.py
+++ crawler.py
@@ -4,31 +4,54 @@ Crawler classes for going through CoC homepage and business site
 import requests
 from bs4 import BeautifulSoup
 import time
+from urllib.parse import urlparse
 
 
 class CocCrawler:
     def __init__(self, coc_url='http://business.andersonville.org/list'):
         self.coc_url = coc_url
         # Main object that stores business site, links, and points of contact
-        # ['biz1_url': {
-        #     'links': ['biz1_url/path1', 'biz1_url/path2', 'biz1_url/path3'],
-        #     'email': ['info@biz.com', 'info2@biz.com'],
-        #     'phone': ['1234567', '9000000']
-        # }]
-        self.biz_info = []
+        # {'biz1_url': {
+        #     'links': {'biz1_url/path1', 'biz1_url/path2', 'biz1_url/path3'},
+        #     'email': {'info@biz.com', 'info2@biz.com'},
+        #     'phone': {'1234567', '9000000'}
+        # }}
+        self.biz_info = {}
+
+    def get_domain_links(self, url):
+        """
+        Get all links from a page
+        :return links: list
+        """
+        domain = urlparse(url).netloc
+        links = self.get_links(url)
+        d_links = [link for link in links if domain in link]
+        return d_links
+
+    def get_links(self, url):
+        """
+        Get
+        :param url: str
+        :return links: list
+        """
+        print('Getting link {}'.format(url))
+        session = requests.get(self.coc_url)
+        soup = BeautifulSoup(session.text)
+        links = [link.get('href') for link in soup.find_all('a')]
+        print('Sleeping...')
+        time.sleep(8)
+        return links
 
     def crawl_main(self):
         """
         Crawl main page of Chamber of Commerce
         """
-        session = requests.get(self.coc_url)
-        soup = BeautifulSoup(session.text)
-        links = [link.get('href') for link in soup.find_all('a')]
+        links = self.get_links(self.coc_url)
         link_set = {link for link in links if 'ql' in link}
         while link_set:
             print('Sleeping')
             time.sleep(10)
-            sub_link = link_set.pop(0)
+            sub_link = link_set.pop()
             print('Getting sub-site from {}'.format(sub_link))
             self.crawl_sub(sub_link)
         print('Found all sub_links')
@@ -37,14 +60,12 @@ class CocCrawler:
         """
         Crawl sub page of Chamber of Commerce site that points to business pages
         """
-        r_sub = requests.get(self.coc_url)
-        soup = BeautifulSoup(r_sub.text)
-        links = [link.get('href') for link in soup.find_all('a')]
+        links = self.get_links(sub_link)
         sb_links = {link for link in links if 'member' in link}
         while sb_links:
             print('Sleeping')
             time.sleep(10)
-            b_link = sb_links.pop(0)
+            b_link = sb_links.pop()
             print('Getting sub biz link from {}'.format(b_link))
             r_biz = requests.get(b_link)
             biz_soup = BeautifulSoup(r_biz.text)
@@ -52,26 +73,35 @@ class CocCrawler:
                 if l.get_text() == 'Visit Website':
                     biz_link = l.get('href')
                     print('Found biz_link {}'.format(biz_link))
-                    self.biz_info.append({biz_link: {'links': [], 'email': [], 'phone': []}})
+                    self.biz_info.append({biz_link: {'links': set(), 'email': set(), 'phone': set()}})
                     break
-        # TODO call crawl_biz()
 
-    def crawl_biz(self, biz_url, crawled):
+    def crawl_biz(self, biz_url, limit=20):
         """
         Recursively crawl business page to get contact info
         """
-        # TODO while loop to check if still left to crawl
-        # TODO get session for biz site
-        # TODO look for all contact links and put in self.biz_info
-        # TODO look for all domain links and put in self.biz_info
-        # TODO track visited sites
-        # TODO sleep
-        # TODO recursively crawl
-        ...
+        to_crawl = {biz_url}
+        crawled = set()
+        counter = 0
+        # check if difference of crawled and sites to crawl is nothing
+        while to_crawl - crawled and counter < limit:
+            biz_link = to_crawl.pop()
+            links = self.get_links(biz_link)
+            d_links = {link for link in links if 'ql' in link}
+            to_crawl.update(d_links)
+            emails = {link for link in links if 'mailto:' in link}
+            phones = {link for link in links if 'tel:' in link}
+
+            self.biz_info[biz_url]['email'].update(emails)
+            self.biz_info[biz_url]['phone'].update(phones)
+            self.biz_info[biz_url]['links'].update(d_links)
+
+            crawled.add(biz_link)
+            counter += 1
 
     def run(self):
         self.crawl_main()
-        for biz_link in self.biz_info:
+        for biz_link in self.biz_info.keys():
             self.crawl_biz(biz_link)
 
 
