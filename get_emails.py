3214550882f49b89bf9d7067cb1fa2f2523c0238
Ryan Anderson <randerson325@comcast.net>
2021-07-22 21:59:43 -0500

    Strip site

--- get_emails.py
+++ get_emails.py
@@ -5,14 +5,14 @@ DATA_PATH = os.environ.get('DATA_PATH', '/var/opt')
 
 def all_emails():
     crawled_sites = os.listdir(DATA_PATH)
-    crawled_sites = [site.strip('biz_').strip('.json') for site in crawled_sites]
     emails = {}
     for site in crawled_sites:
-        if site != 'sites':
+        if site != 'biz_sites.json':
             with open(site, 'r') as file:
                 text = file.read()
             data = eval(text)
-            emails.update({site: data['email']})
+            strip_site = site.strip('biz_').strip('.json')
+            emails.update({strip_site: data['email']})
     return emails
 
 
